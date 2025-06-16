9171ecec279e430c2f2035ad527c2d101abcbce8
dat94-03 <tiendat942003@gmail.com>
2025-06-16 15:15:15 +0700

    Backup before cleanup

--- config.py
+++ config.py
@@ -1,14 +1,15 @@
 import torch
 
 # Paths
-DATA_SET = 'name_of_the_dataset_here'
+DATA_SET = 'Potato_splited'
 DATA_PATH=f"./data/{DATA_SET}"
 TRAIN_DATA_PATH = f"{DATA_PATH}/train"
 VAL_DATA_PATH = f"{DATA_PATH}/validation"
 TEST_DATA_PATH = f"{DATA_PATH}/test"
-MODEL_NAME=f'set_name_for_the_model_here+{DATA_SET}'
-MODEL_PATH = f"./output/models/{MODEL_NAME}+{DATA_SET}.pth"
+MODEL_NAME=f'ConvNeXt+{DATA_SET}'
+MODEL_PATH = f"./output/models/{MODEL_NAME}.pth"
 PATIENCE =10
+
 # Hyperparameters
 BATCH_SIZE = 16
 LEARNING_RATE = 0.0001
